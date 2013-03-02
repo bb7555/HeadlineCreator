39c3180d713cbe3b69602212b3fd8a7eb00f7202
bb7555 <cliffbarnes@live.com>
2013-03-02 00:44:59 -0500

    March 1st progress on application

--- js/headline.creator.js
+++ js/headline.creator.js
@@ -3,22 +3,53 @@
 
 (function( $ ) {
   $.fn.HeadlineCreator = function() {
-	  
-	  var part1 = $(".part-1");
+	
+	
+	//set selector objects  
+	var part1 = $(".part-1");
 	var part2 = $(".part-2");
 	var continue_to_step_2 = $("#continue_to_step_2");
 	var continue_to_step_1 = $("#continue_to_step_1");
+	var loading = $(".loading");
+	var headline_2 = $("#headline_2");
+	var headline_3 = $("#headline_3");
+	var headline3 = $(".headline3");
+	var banner_yes = $("#banner_yes");
+	var banner_no = $("#banner_no");
+	var newspaper_banner = $("#newspaper_banner");
 	
+	///FIRST, get rid of the loader screen
+	setTimeout(loading.fadeOut("slow"), 3000);
+	
+	/////Toggle Number of Headlines
+	headline_3.click(function(){
+		headline3.fadeIn("slow");
+		});
+	headline_2.click(function(){
+		headline3.fadeOut("slow");
+		});	
+	
+	
+	//////Banner UI Event Listeners//////////
+	banner_yes.click(function(){
+	
+		newspaper_banner.fadeIn("slow");
+	});
+	banner_no.click(function(){
+		newspaper_banner.fadeOut("slow");
+	});
+	
+	//Allow Transitions Beteen the two steps
 	continue_to_step_2.click(function(){
 		
-		part1.fadeOut();
-		part2.fadeIn();
+		part1.fadeOut(function(){part2.fadeIn();});
+		
 		})
 		
 	continue_to_step_1.click(function(){
 		
-		part2.fadeOut();
-		part1.fadeIn();
+		part2.fadeOut(function(){part1.fadeIn();});
+		
 		})	
    
 
